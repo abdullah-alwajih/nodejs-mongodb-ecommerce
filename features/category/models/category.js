252c700cfbdf23a9e26f910708996699e81501db
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-08-21 23:37:30 +0300

    create category schema and model

--- features/category/models/category.js
+++ features/category/models/category.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  name: String
-});
+  name: {
+    type: String,
+    required: [true, 'Category required'],
+    unique: [true, 'Category required'],
+    minLength: [3, 'Too short category name'],
+    maxLength: [32, 'Too long category name']
+  },
+  // A and B => shopping.com/a-and-b
+  slug: {
+    type: String, lowercase: true,
+  },
+}, {timestamps: true,});
 
 const CategoryModel = mongoose.model('Category', categorySchema);
 
