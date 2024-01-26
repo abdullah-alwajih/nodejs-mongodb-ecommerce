8800e85fed666fffc138693bdbc69c61962fabb8
abdullah-alwajih <pro.abdullah.alwajih@gmail.com>
2024-01-26 19:02:45 +0300

    add domain to cors origins

--- core/config/cors_origins.js
+++ core/config/cors_origins.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const ApiError = require("../base/models/apiError");
 
 const initCROSOrigins = (app) => {
-  const whitelist = ['http://example1.com', 'http://example2.com']
+  const whitelist = ['https://api-ecommerce-abdullah.vercel.app', 'http://example2.com']
   const corsOptions = {
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1 || !origin) {
