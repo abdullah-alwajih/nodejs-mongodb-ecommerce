e1b8362c1d122cfdf6805ae3b1ffb92655b894ec
abdullah-alwajih <pro.abdullah.alwajih@gmail.com>
2024-01-30 04:48:21 +0300

    Take precautions against brute-forcing

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const globalError = require("./core/middlewares/errorMiddleware");
 const initRoutes = require("./core/config/routes");
 const initCROSOrigins = require("./core/config/cors_origins");
 const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const {json} = require("express");
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'uploads')));
@@ -26,6 +28,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // compress all responses
 app.use(compression())
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  handler: (req, res, /*next*/) => {
+    res.status(429).json({
+      message: "Too many requests, please try again later."
+    });
+  },
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use('/api', limiter);
+
 initRoutes(app);
 
 
